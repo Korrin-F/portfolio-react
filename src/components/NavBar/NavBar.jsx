a25d7c6b104279ad0d406c79e14bbfdf53fbd4e6
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-06 01:58:04 +0000

    update home and navbar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,19 +4,19 @@ function NavBar() {
     const textLogo = "{K:F}";
     return (
        
-        <nav class="navbar navbar-expand-lg fixed-top  nav-cont">
-            <div class="container-fluid ">
-                <a class="navbar-brand d-flex align-content-center " href="#top">
-                    <h3 class="my-0">{textLogo}</h3>
+        <nav className="navbar navbar-expand-lg fixed-top  nav-cont">
+            <div className="container-fluid ">
+                <a className="navbar-brand d-flex align-content-center " href="#top">
+                    <h3 className="my-0">{textLogo}</h3>
                 </a>
-                <button class="navbar-toggler me-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler me-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-lg-end" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                    <a class="nav-link" aria-current="page" href="#about">About Me</a>
-                    <a class="nav-link" href="#work">Work</a>
-                    <a class="nav-link" href="#contact">Contact</a>
+                <div className="collapse navbar-collapse justify-content-lg-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                    <a className="nav-link" aria-current="page" href="#about">About Me</a>
+                    <a className="nav-link" href="#work">Work</a>
+                    <a className="nav-link" href="#contact">Contact</a>
                     </div>
                 </div>
             </div>
