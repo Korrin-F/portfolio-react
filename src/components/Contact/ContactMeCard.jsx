37ae4cd0c5c2a8484641c7cdfb3b858bfdf8cb44
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-10 18:19:21 +0000

    add linkedIn and github links

--- src/components/Contact/ContactMeCard.jsx
+++ src/components/Contact/ContactMeCard.jsx
@@ -8,12 +8,12 @@ function ContactMeCard() {
                 <h3 className="display-6 px-3 mb-0 mx-auto">Contact Me</h3>
                 <div className="row contact-content p-4 py-5 p-sm-5 ">
                     <div className="col-6 ">
-                        <a className="btn button" href="" target="_blank">
+                        <a className="btn button" href="https://www.linkedin.com/in/korrin-franklin/" target="_blank">
                             LinkedIn
                         </a>
                     </div>
                     <div className="col-6 ">
-                        <a className="btn button" href="" target="_blank">
+                        <a className="btn button" href="https://github.com/korrin-f" target="_blank">
                             GitHub
                         </a>
                     </div>
