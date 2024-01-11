88a4845be918da7fb1b37d327c37872fe48d0f3a
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-11 02:28:28 +0000

    updated key value JS styling

--- src/components/Values/Value.jsx
+++ src/components/Values/Value.jsx
@@ -1,14 +1,30 @@
 import Circle from "./Circle";
 import './Value.css'
 
-
+const styles = {
+    p: {
+        fontFamily: 'var(--font-family-secondary)',
+        color: 'var(--dark)'
+    }
+}
 function Value(props) {
+    const {title, text, id, key} = props;
+
     return (
-        <div className="value col-10 col-sm-7 col-lg-4 py-4 px-2">
+        <div className="value col-10 col-sm-7 col-lg-4 py-4 px-2" key={key}>
             <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>               
-                <p className="card-text">{props.text}</p>                 
-                <Circle id={props.id} />
+                <h5 
+                className="card-title"
+                >
+                    {title}
+                </h5>               
+                <p 
+                className="card-text"
+                style={styles.p}
+                >
+                    {text}
+                </p>                 
+                <Circle id={id} />
             </div>     
         </div>
     )
